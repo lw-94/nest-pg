ad73bc8c6d21d84a8c53ebf48aa78a77bf76eadd
bv <>
2024-04-09 14:46:33 +0800

    update

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import type { CreateUserDto } from './dto/create-user.dto'
+import type { UpdateUserDto } from './dto/update-user.dto'
 
 @Injectable()
 export class UserService {
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user:' + `${JSON.stringify(createUserDto)}`
+    return `This action adds a new user: ${JSON.stringify(createUserDto)}`
   }
 
   findAll() {
@@ -15,8 +16,8 @@ export class UserService {
     return `This action returns a #${id} user`
   }
 
-  update(id: number) {
-    return `This action updates a #${id} user`
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user: ${JSON.stringify(updateUserDto)}`
   }
 
   remove(id: number) {
